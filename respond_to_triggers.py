00c01ee5d2f84d4b5bf0354c607bc7812f42e98b
Stefan Armbruster <stefan@armbruster-it.de>
2017-10-22 14:30:21 +0200

    using named pipe

--- respond_to_triggers.py
+++ respond_to_triggers.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python
 
 import os, wiringpi
+from neo4j.v1 import GraphDatabase
 
+pipe_name = '/tmp/button_to_cypher_pipe'
+if not os.path.exists(pipe_name):
+    os.mkfifo(pipe_name)
 
-# def gpio_callback_2():
-#     print "GPIO_CALLBACK write!"
-#     with driver.session() as session:
-#         session.write_transaction(add_node)
+if os.fork() == 0:  
+    # listener
+    pipein = open(pipe_name, 'r')
+    driver = GraphDatabase.driver("bolt+routing://192.168.178.100:7687" )
+    #driver = GraphDatabase.driver("bolt://192.168.178.100:7687")
+    # while True:
+    #     command = pipein.readline()[:-1]
+    #     print("received %s." % (command))
+    #     mode = WRITE_ACCESS if command=='write' else READ_ACCESS 
+    #     cypher = "create ()" if command=='write' else 'match (n) return count(n) as count'
 
-# def gpio_callback_3():
-#     print "GPIO_CALLBACK read!"
-#     with driver.session() as session:
-#         print "session"
-#         count = session.read_transaction(count_nodes)
-#         print "we have %d nodes" % (count)
+    #     with driver.session(mode) as session:
+    #         print session.run(cypher).single()[0]
 
+    while True:
+        line = pipein.readline()[:-1]
+        print 'Parent %d got "%s" at %s' % (os.getpid(), line, time.time( ))
 
-def child(pipeout):
+else: 
+    # writer
+    pipeout = os.open(pipe_name, os.O_WRONLY)
     wiringpi.wiringPiSetupGpio()
-    #wiringpi.pinMode(PIN_TO_SENSE, wiringpi.GPIO.INPUT)
+    wiringpi.pinMode(2, wiringpi.GPIO.INPUT)
+    wiringpi.pinMode(3, wiringpi.GPIO.INPUT)
     wiringpi.pullUpDnControl(2, wiringpi.GPIO.PUD_UP)
     wiringpi.pullUpDnControl(3, wiringpi.GPIO.PUD_UP)
 
@@ -27,29 +39,8 @@ def child(pipeout):
     while True:
         wiringpi.delay(2000)
 
-pipein, pipeout = os.pipe()
-if os.fork() == 0:  
-    os.close(pipein)
-    child(pipeout)
-
-else: # reader process -> send cypher queries
-    os.close(pipeout)
-    pipein = os.fdopen(pipein)
-    driver = GraphDatabase.driver("bolt+routing://92.168.178.100:7687" )
-    #driver = GraphDatabase.driver("bolt://192.168.178.100:7687")
-    while True:
-        command = pipein.readline()[:-1]
-        print("received %s." % (command))
-        mode = WRITE_ACCESS if command=='write' else READ_ACCESS 
-        cypher = "create ()" if command=='write' else 'match (n) return count(n) as count'
-
-        with driver.session(mode) as session:
-            print session.run(cypher).single()[0]
-
-
 # import wiringpi
 # from time import sleep
-# from neo4j.v1 import GraphDatabase
 
 # PIN_TO_SENSE = 3
 
